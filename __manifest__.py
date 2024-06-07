9923d893b36585c6be9bb73f0e695f3f37161e3f
Eco-Clic <tecnico@eco-clic.net>
2024-06-07 12:18:14 +0200

    __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Albaranes personalizados',
+    'version': '1.0',
+    'depends': ['base'],
+    'author': 'Eco-Clic',
+    'description': """
+    Módulo para facturas y albaranes personalizados
+    """,
+    'data': [
+        'views/boton_albaranes_personalizadas.xml',
+        'reports/report_albaranes_personalizadas.xml',
+    ],
+    'installable': True,
+}
